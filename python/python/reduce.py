e01e3e6bd962e97f67acd10d431fed137086704d
Romain <5385463+rfldn@users.noreply.github.com>
2023-11-14 13:59:20 +0100

    refactor

--- python/python/reduce.py
+++ python/python/reduce.py
@@ -5,7 +5,7 @@ ITERATIONS = 60000
 
 
 def reduce_huge_list():
-    return reduce(lambda acc, index: acc + [index], range(ITERATIONS), [])
+    return reduce(lambda acc, index: [*acc, index], range(ITERATIONS), [])
 
 
 def reduce_huge_list_mutable():
